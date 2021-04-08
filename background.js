86775c7cd236eae35266a0c886154463280582c8
Yuyu <ujunglim@naver.com>
2021-04-08 15:31:20 +0800

    fetch bg_img, link, location, photographer

--- background.js
+++ background.js
@@ -1,21 +1,44 @@
+const UNSPLASH_KEY = "gEWMDZqOynH1rpYb60vKEajc-a9m0I27Ccxgf4njbSA";
+// DOM
 const body = document.querySelector("body");
+const linkDOM = document.querySelector(".bg_link");
+const locationDOM = document.querySelector(".bg_location");
+const photographerDOM = document.querySelector(".bg_photographer");
 
-const IMG_NUMBER = 10;
-
-const getRandomNum = () => {
-  return Math.ceil(Math.random() * IMG_NUMBER);
+const showBGImage = (img) => {
+  const url = img.urls.regular;
+  body.style.backgroundImage = `url("${url}")`;
 }
 
-const showImage = (numb) => {
-  const image = new Image();
-  image.src = `./image/${numb}.jpg`;
-  image.classList.add('backgroundImage');
-  body.appendChild(image);
+const showBGImageInfo = (img) => {
+  const link = img.links.html;
+  let location = img.user.location;
+  const photographer = img.user.name;
+
+  if(!location) {
+    location = "Unsplash nature";
+  }
+
+  linkDOM.href = link;
+  locationDOM.innerText = location;
+  photographerDOM.innerText = photographer;
+}
+
+const loadBGImage = () => {
+  fetch(`https://api.unsplash.com/search/photos/?query=nature&color=black&orientation=landscape&client_id=${UNSPLASH_KEY}`)
+  .then(response => {return response.json()})
+  .then(data => {
+    const images = data.results;
+    const index = Math.floor(Math.random() * images.length);
+    const img = images[index];
+
+    showBGImage(img);
+    showBGImageInfo(img);
+  })
 }
 
 function init() {
-  const randomNum = getRandomNum();
-  showImage(randomNum);
+  loadBGImage();
 }
 
 init();
