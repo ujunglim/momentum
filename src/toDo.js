1a2e54180071a29f881db2d215cf416c797abc32
Yuyu <ujunglim@naver.com>
2021-04-12 15:51:18 +0800

    edit todo(textarea)

--- src/toDo.js
+++ src/toDo.js
@@ -29,21 +29,34 @@ const deleteToDo = (event) => {
   saveStorage();
 }
 
+const onExpandTextarea = ({ target }) => {
+  target.rows = Math.ceil(target.value.length / 30);
+}
+
 const editToDo = (event) => {
   const li = event.target.parentNode;
   const content = li.querySelector(".toDoContent");
   content.removeAttribute("disabled");
   content.focus();
   content.classList.add("editing");
+  content.rows = Math.ceil(content.value.length / 30);
 
+  content.addEventListener('input', onExpandTextarea)
   content.addEventListener("keypress", (event) => {
     if(event.key === "Enter") {
-      toDos[li.id].text = content.value;
+      // when input is empty
+      if(!event.target.value) {
+        content.value = toDos[li.id].text;
+      }
+      else {
+        toDos[li.id].text = content.value;
+      } 
       saveStorage();
       content.disabled = "true";
       content.classList.remove("editing");
     }
   });
+
 }
 
 // create list 
@@ -57,11 +70,11 @@ const showToDo = (text) => {
   const label = document.createElement('label');
   label.for = id;
 
-  const content = document.createElement('input');
-  content.className = "toDoContent"
-  content.maxLength = 30;
+  const content = document.createElement('textarea');
+  content.className = "toDoContent";
   content.value = text;
   content.disabled = "true";
+  content.rows = Math.ceil(content.value.length / 30);
 
   const deleteBtn = document.createElement('button');
   const editBtn = document.createElement('button');
@@ -88,6 +101,7 @@ const showToDo = (text) => {
   // save to localStorage
   saveStorage();
 }
+
 
 const onSubmitToDo = (event) => {
   event.preventDefault();
